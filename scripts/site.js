c9513833a6a5d5a97a49a53d7f8d2bb47acd36ee
Vanessa Barreiros <vanessa@vinta.com.br>
2019-04-12 16:44:58 -0300

    Adiciona navegação no menu

--- scripts/site.js
+++ scripts/site.js
@@ -1,11 +1,18 @@
-import $ from "jquery";
+import mPageScroll2id from "page-scroll-to-id";
 
 $(document).ready(() => {
   activateMenuToggle();
+  activateOneScroll();
 });
 
 const activateMenuToggle = () => {
   const menu = $("#pyn-nav");
   const toggle = $("#toggleMenu");
   toggle.click(() => menu.toggleClass("is-open"));
+};
+
+const activateOneScroll = () => {
+  $(".navbar a[href*='#']").mPageScroll2id({
+    offset: 80
+  });
 };
